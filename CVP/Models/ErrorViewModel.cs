0222809d90a11cb47882366adfedfe019eff305d
JoseJG77 <dajosejr@gmail.com>
2025-04-11 21:00:43 -0600

    Agregar archivos de proyecto.

---
+++ CVP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CVP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
